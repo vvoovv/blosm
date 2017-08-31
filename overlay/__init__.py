2ed3d0a45f490c4281b48e5fb0ce9d79715378ee
Vladimir Elistratov <prokitektura+dev@gmail.com>
2017-09-01 00:20:32 +0300

    tile stitching seems to work

--- overlay/__init__.py
+++ overlay/__init__.py
@@ -1,4 +1,5 @@
 import math, os
+import numpy
 from urllib import request
 import bpy
 
@@ -29,6 +30,8 @@ class Overlay:
     maxNumTiles = 256 # i.e. 4096x4096 pixels
     
     tileCoordsTemplate = "{z}/{x}/{y}"
+    
+    blenderImageName = "overlay"
     
     def __init__(self, url, maxZoom, addonName):
         self.maxZoom = maxZoom
@@ -67,11 +70,22 @@ class Overlay:
         self.urlEnd = urlEnd
     
     def doImport(self, l, b, r, t):
-        def toTileCoords(coord, zoom):
+        def toTileCoord(coord, zoom, tileSize=0):
             """
             An auxiliary function used below in the code
+            
+            Returns:
+            A single integer tile coordinate if <tileSize>==0.
+            A Python tuple with two elements otherwise:
+                0) a single integer tile coordinate
+                1) number of pixels converted from the fractional part of the tile coordinate,
+                    using <tileSize>
             """
-            return int(math.floor(coord * math.pow(2, zoom) / equator))
+            coord = coord * math.pow(2, zoom) / equator
+            floor = math.floor(coord)
+            return ( int(floor), int(math.ceil((coord - floor) * tileSize)) )\
+                if tileSize else\
+                int(floor)
         
         # Convert the coordinates from degrees to spherical Mercator coordinate system
         # and move zero to the top left corner (that's why the 3d argument in the function below)
@@ -89,40 +103,54 @@ class Overlay:
             _zoom = zoom + 1
             while _zoom <= self.maxZoom:
                 # convert <l>, <b>, <r>, <t> to tile coordinates
-                _l, _b, _r, _t = tuple(toTileCoords(coord, _zoom) for coord in (l, b, r, t))
+                _l, _b, _r, _t = tuple(toTileCoord(coord, _zoom) for coord in (l, b, r, t))
                 if (_r - _l + 1) * (_b - _t + 1) > self.maxNumTiles:
                     break
                 zoom = _zoom
                 _zoom += 1
         
         # convert <l>, <b>, <r>, <t> to tile coordinates
-        l, b, r, t = tuple(toTileCoords(coord, zoom) for coord in (l, b, r, t))
+        l, b, r, t = tuple(toTileCoord(coord, zoom, self.tileWidth) for coord in (l, b, r, t))
+        numTilesX = r - l + 1
+        numTilesY = b - t + 1
+        # a numpy array for the resulting image stitched out of all tiles
+        imageData = numpy.ones(4*numTilesX*self.tileWidth * numTilesY*self.tileHeight)
+        w = 4 * self.tileWidth
         # get individual tiles
         for x in range(l, r+1):
             for y in range(t, b+1):
-                tile = self.getTileImage(zoom, x, y)
-        #bpy.data.images.new(
-        #    "MyImage",
-        #    width = (r - l + 1) * self.tileWidth,
-        #    height = (b - t + 1) * self.tileHeight
-        #)
+                tileData = self.getTileData(zoom, x, y)
+                for _y in range(self.tileHeight):
+                    i1 = w * ( (numTilesY-1-y+t) * self.tileHeight*numTilesX + _y*numTilesX + x-l )
+                    imageData[i1:i1+w] = tileData[_y*w:(_y+1)*w]
+        # create the resulting Blender image stitched out of all tiles
+        image = bpy.data.images.new(
+            self.blenderImageName,
+            width = (r - l + 1) * self.tileWidth,
+            height = (b - t + 1) * self.tileHeight
+        )
+        image.pixels = imageData
     
-    def getTileImage(self, zoom, x, y):
+    def getTileData(self, zoom, x, y):
         # check if we the tile in the file cache
         j = os.path.join
         tileDir = j(self.overlayDir, str(zoom), str(x))
         tilePath = j(tileDir, "%s.%s" % (y, self.imageExtension))
-        if os.path.exists(tilePath):
-            return None
-        else:
-            tile = request.urlopen(self.getTileUrl(zoom, x, y)).read()
+        if not os.path.exists(tilePath):
+            tileData = request.urlopen(self.getTileUrl(zoom, x, y)).read()
             # ensure that all directories in <tileDir> exist
             if not os.path.exists(tileDir):
                 os.makedirs(tileDir)
             # save the tile to file cache
             with open(tilePath, 'wb') as f:
-                f.write(tile)
-        return tile
+                f.write(tileData)
+        # Create a temporary Blender image out of the tile image
+        # to create a numpy array out of the image raw data
+        tmpImage = bpy.data.images.load(tilePath)
+        tileData = numpy.array(tmpImage.pixels)
+        # delete the temporary Blender image
+        bpy.data.images.remove(tmpImage, True)
+        return tileData
         
     
     def getOverlaySubDir(self):
