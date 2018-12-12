fb529650c805843037da1e9b344dcf36be8743b1
Vladimir Elistratov <prokitektura+dev@gmail.com>
2018-12-12 18:59:08 +0300

    Blender version in bl_info

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@ bl_info = {
     "name": "Import OpenStreetMap (.osm) (Premium)",
     "author": "Vladimir Elistratov <prokitektura+support@gmail.com>",
     "version": (1, 0, 11),
-    "blender": (2, 7, 9),
+    "blender": (2, 80, 0),
     "location": "Left side panel for Blender 2.79 (right side panel for Blender 2.8)> \"osm\" tab",
     "description": "One click download and import of OpenStreetMap, terrain, satellite imagery, web maps",
     "warning": "",
