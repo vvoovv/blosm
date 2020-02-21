b93ae4886baec76fc8d9d33052212d655deb637c
Vladimir Elistratov <prokitektura+dev@gmail.com>
2020-02-21 12:39:04 +0300

    (premium)v1.0.25

--- __init__.py
+++ __init__.py
@@ -18,14 +18,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 bl_info = {
-    "name": "Import OpenStreetMap (.osm)",
+    "name": "Import OpenStreetMap (.osm) (Premium)",
     "author": "Vladimir Elistratov <prokitektura+support@gmail.com>",
-    "version": (2, 3, 21),
+    "version": (1, 0, 25),
     "blender": (2, 80, 0),
-    "location": "Right side panel for Blender 2.80 (left side panel for Blender 2.79)> \"osm\" tab",
-    "description": "One click download and import of OpenStreetMap and terrain",
+    "location": "Right side panel for Blender 2.8x (left side panel for Blender 2.79))> \"osm\" tab",
+    "description": "One click download and import of OpenStreetMap, terrain, satellite imagery, web maps",
     "warning": "",
-    "wiki_url": "https://github.com/vvoovv/blender-osm/wiki/Documentation",
+    "wiki_url": "https://github.com/vvoovv/blender-osm/wiki/Premium-Version",
     "tracker_url": "https://github.com/vvoovv/blender-osm/issues",
     "support": "COMMUNITY",
     "category": "Import-Export"
