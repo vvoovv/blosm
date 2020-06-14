4a795ab03638b3708629a6755580bbacb4280e83
Zkir <zkir@zkir.ru>
2020-06-14 23:38:41 +0300

    Test.bat removed

--- test.bat
+++
@@ -1,2 +0,0 @@
-"c:\Program Files\Python37\python.exe" pml/PML2PythonTranslator.py "c:\Users\zkir\AppData\Roaming\Blender Foundation\Blender\assets\style\building\place_of_worship.pml" 
-rem > output.txt
