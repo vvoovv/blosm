0f2c7d866a546569a391276eff4dab0a081e9300
Vladimir Elistratov <prokitektura+dev@gmail.com>
2020-07-05 13:10:56 +0300

    aditional validity check

--- action/volume/roof_flat.py
+++ action/volume/roof_flat.py
@@ -21,6 +21,11 @@ class RoofFlat(Roof):
         self.extrudeTillRoof = False
     
     def render(self, footprint):
+        if footprint.noWalls:
+            # additional validity check here
+            # that case can't happen for buildings with the flat roof
+            footprint.valid = False
+            return
         # <firstVertIndex> is the index of the first vertex of the polygon that defines the roof base;
         # since it depends on the total number of building vertices, we calculated it before any operation
         # that creates building geometry 
