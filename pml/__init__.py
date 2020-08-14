71c39c1a520dde86bd4f1644a834e8aa42e90565
Vladimir Elistratov <prokitektura+dev@gmail.com>
2020-08-14 13:03:36 +0300

    PML update

--- pml/__init__.py
+++ pml/__init__.py
@@ -1,23 +1,17 @@
-from antlr4 import InputStream, CommonTokenStream, ParseTreeWalker
+from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker
 from .pml_grammar.pmlLexer import pmlLexer
 from .pml_grammar.pmlParser import pmlParser
 from .PythonListener import PythonListener
-from .PML_Preprocessor import PML_Preprocessor
 from .ExceptionManagement import ParserExceptionListener
-from .ExceptionManagement import ParserException
 
 
 class PML:
     
-    def __init__(self, pmlFilePath, assetsDir):
+    def __init__(self, pmlFilePath):
         self.pmlFilePath = pmlFilePath
-        self.assetsDir = assetsDir
     
     def getPythonCode(self):
-        preprocessor = PML_Preprocessor(self.assetsDir)
-        preprocessor.process(self.pmlFilePath)
-        
-        inputStream = InputStream(preprocessor.getStream())
+        inputStream = FileStream(self.pmlFilePath)
         lexer = pmlLexer(inputStream)
         stream = CommonTokenStream(lexer)
         parser = pmlParser(stream)
@@ -26,19 +20,8 @@ class PML:
         exceptionListener = ParserExceptionListener()
         parser.addErrorListener(exceptionListener)
         
-        try:
-            tree = parser.styles()
-        except ParserException as e:
-            line, col, msg = e.errParams()
-            localFile, localLine = preprocessor.trackDownLineNr(line)
-            raise Exception(
-                "Error in file {file} on line {line}, col {col}: {msg}".format(
-                    file = localFile, line = localLine, col = col, msg = msg
-                )
-            )
-        
+        tree = parser.styles()
         translator = PythonListener()
         walker = ParseTreeWalker()
         walker.walk(translator, tree)
-        
         return translator.getCode()
