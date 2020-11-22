d191c9638f80ceaf38646ff7c090d908c907483d
Vladimir Elistratov <prokitektura+dev@gmail.com>
2020-11-22 20:18:55 +0300

    v2.4.29

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 bl_info = {
     "name": "blender-osm",
     "author": "Vladimir Elistratov <prokitektura+support@gmail.com>",
-    "version": (2, 4, 28),
+    "version": (2, 4, 29),
     "blender": (2, 80, 0),
     "location": "Right side panel > \"osm\" tab",
     "description": "One click download and import of OpenStreetMap, terrain, satellite imagery, web maps",
