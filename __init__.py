79ff2ed5712e68f815efefbd8a9d0110e1c549f0
Vladimir Elistratov <prokitektura+dev@gmail.com>
2021-02-10 20:12:47 +0300

    integrating the asset package editor

--- __init__.py
+++ __init__.py
@@ -70,7 +70,7 @@ class BlosmPreferences(bpy.types.AddonPreferences):
     bl_idname = __name__
     
     screenType: bpy.props.EnumProperty(
-        name = '',
+        name = "Screen type for addon preferences",
         items = (
             ("preferences", "preferences", "preferences"),
             ("ape", "asset package editor", "asset package editor")
