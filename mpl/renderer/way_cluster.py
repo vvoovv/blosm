10b7d14dd480574202e834fc29ee69e0a405aac6
Vladimir Elistratov <prokitektura+dev@gmail.com>
2021-09-30 14:54:12 +0300

    matplotlib styling

--- mpl/renderer/way_cluster.py
+++ mpl/renderer/way_cluster.py
@@ -1,11 +1,66 @@
 from math import sqrt
 from . import Renderer
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as pe
 
 
 class WayClusterRenderer(Renderer):
     
+    styles = dict(
+        primary = dict(
+            color="#fcd6a4", linewidth=5.,
+            path_effects=[pe.Stroke(linewidth=5.4, foreground="black"), pe.Normal()]
+        ),
+        primary_link = dict(
+            color="#fcd6a4", linewidth=5.,
+            path_effects=[pe.Stroke(linewidth=5.4, foreground="black"), pe.Normal()]
+        ),
+        secondary = dict(
+            color="#f7fabf",
+            linewidth=4.,
+            path_effects=[pe.Stroke(linewidth=4.4, foreground="black"), pe.Normal()]
+        ),
+        secondary_link = dict(
+            color="#f7fabf",
+            linewidth=4.,
+            path_effects=[pe.Stroke(linewidth=4.4, foreground="black"), pe.Normal()]
+        ),
+        tertiary = dict(
+            color="#ffffff", linewidth=3.,
+            path_effects=[pe.Stroke(linewidth=3.4, foreground="black"), pe.Normal()]
+        ),
+        tertiary_link = dict(
+            color="#ffffff", linewidth=3.,
+            path_effects=[pe.Stroke(linewidth=3.4, foreground="black"), pe.Normal()]
+        ),
+        residential = dict(
+            color="#ffffff", linewidth=3.,
+            path_effects=[pe.Stroke(linewidth=3.4, foreground="black"), pe.Normal()]
+        ),
+        unclassified = dict(
+            color="#ffffff", linewidth=3.,
+            path_effects=[pe.Stroke(linewidth=3.4, foreground="black"), pe.Normal()]
+        ),
+        pedestrian = dict(
+            color="#dddde8", linewidth=3.,
+            path_effects=[pe.Stroke(linewidth=3.4, foreground="black"), pe.Normal()]
+        ),
+        living_street = dict(
+            color="#ededed", linewidth=3.,
+            path_effects=[pe.Stroke(linewidth=3.4, foreground="black"), pe.Normal()]
+        ),
+        service = dict(
+            color="#ffffff", linewidth=2.,
+            path_effects=[pe.Stroke(linewidth=2.4, foreground="black"), pe.Normal()]
+        ),
+        footway = dict(color="#fa8072", linewidth=1., linestyle="dashed"),
+        steps = dict(color="#fa8072", linewidth=2., linestyle="dotted"),
+        path = dict(color="#fa8072", linewidth=1., linestyle="dashed"),
+        cycleway = dict(color="#0000ff", linewidth=1., linestyle="dashed")
+    )
+    
     def render(self, wayManager, data):
+        self.mpl.ax.set_facecolor("#f2efe9")
         self.plotWay(wayManager.networkGraph)
         self.plotJunctions(wayManager.waySectionGraph, wayManager.junctions[0], 'red')
         self.plotJunctions(wayManager.waySectionGraph, wayManager.junctions[1], 'limegreen')
@@ -19,9 +74,9 @@ class WayClusterRenderer(Renderer):
             v1 = v[e.s].data
             v2 = v[e.t].data
             self.mpl.ax.plot(
-                [v1[0],v2[0]],
-                [v1[1],v2[1]],
-                'k'
+                (v1[0], v2[0]),
+                (v1[1], v2[1]),
+                **WayClusterRenderer.styles[e.category]
             )
             # x = (v1[0]+v2[0])/2.
             # y = (v1[1]+v2[1])/2.
@@ -49,12 +104,13 @@ class WayClusterRenderer(Renderer):
                 ( min(x)+dx, min(y)+dy ),
                 r,
                 alpha=0.3,
-                color=color
+                color=color,
+                zorder=100
             )) 
             # plt.plot(min(x)+dx,min(y)+dy, 'o', ms=15, markerfacecolor=color, alpha=0.3)#, markeredgecolor='red', markeredgewidth=5)
             for indx in crossing:
                 x,y = v[indx].data[0], v[indx].data[1]
-                ax.scatter(x, y, 30, color=color)
+                ax.scatter(x, y, 30, color=color, zorder=100)
         
         
 def debugPlot( graph, title):
