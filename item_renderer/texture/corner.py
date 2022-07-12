7d90021f922359ec699c3c367ff57748e05cbe52
Vladimir Elistratov <prokitektura+dev@gmail.com>
2022-07-12 22:15:42 +0300

    dealing with corner items

--- item_renderer/texture/corner.py
+++ item_renderer/texture/corner.py
@@ -1,5 +1,6 @@
-from math import sin, radians
+from math import cos, radians
 from operator import itemgetter
+from bisect import bisect_left
 
 from util.blender import linkCollectionFromFile
 from ..util import getFilepath
@@ -17,21 +18,29 @@ class Corner:
         
         leftCorner = item.cornerL
         
-        # Create a key as a z-coordinate of the bottom of <item>. Note that strings as keys
-        # for Python dictionaries and sets work faster than floats
-        cornerKey = str( round(item.building.renderInfo.verts[indices[0]][2], 3) )
-        
         if leftCorner:
             if not facade.cornerInfoL:
-                facade.cornerInfoL = facade.vector.prev.facade.cornerInfoR = set()
+                facade.cornerInfoL = facade.vector.prev.facade.cornerInfoR = {}
             cornerInfo = facade.cornerInfoL
+            # <cornerVert> is a vert at the bottom of the corner item and on its left side
+            cornerVert = item.building.renderInfo.verts[indices[0]]
         else:
             if not facade.cornerInfoR:
-                facade.cornerInfoR = facade.vector.next.facade.cornerInfoL = set()
+                facade.cornerInfoR = facade.vector.next.facade.cornerInfoL = {}
             cornerInfo = facade.cornerInfoR
+            # <cornerVert> is a vert at the bottom of the corner item and on its right side
+            cornerVert = item.building.renderInfo.verts[indices[1]]
+            
+        # Create a key as a z-coordinate of the bottom of <item>. Note that strings as keys
+        # for Python dictionaries and sets work faster than floats
+        cornerKey = str( round(cornerVert[2], 3) )
         
-        if cornerKey in cornerInfo:
-            # the corner item was already inserted in the adjacent facade
+        if not cornerKey in cornerInfo:
+            # The corner was not visited before. We'll store the bottom coordinate of the corner element
+            # that belong to <item.facade>
+            cornerInfo[cornerKey] = cornerVert
+            # <cornerVert> will be used when we encounter the neighbor part of the corner.
+            # it's needed to position the corner asset correctly.
             return
         
         collectionName = assetInfo["collection"]
@@ -47,15 +56,32 @@ class Corner:
             # for the corners with concave angles
             collectionInfo = (
                 collection,
-                [ ( obj, sin(radians(obj["angle"])) ) for obj in collection.objects if obj["angle"]>0.],
-                [ ( obj, sin(radians(obj["angle"])) ) for obj in collection.objects if obj["angle"]<0.]
+                [ ( obj, -cos(radians(obj["angle"])) ) for obj in collection.objects if obj["angle"]>0.],
+                [ ( obj,  cos(radians(obj["angle"])) ) for obj in collection.objects if obj["angle"]<0.]
             )
             collectionInfo[1].sort(key=itemgetter(1))
             collectionInfo[2].sort(key=itemgetter(1))
             self.r.blenderCollections[collectionKey] = collectionInfo
         
-        # If the neighbor corner item in the adjacent facade is visited, not actions will be needed,
-        # since the corner item has been already inserted here.
-        cornerInfo.add(cornerKey)
+        collectionInfo = self.r.blenderCollections[collectionKey]
+        # Find a Blender object in the Blender collection represented by <collectionInfo>
+        # that angle is the closest one to the angle of the corner in question
+        vector = item.facade.vector.next if leftCorner else item.facade.vector
         
+        if vector.sin > 0.:
+            #
+            # convex angle of the corner
+            #
+            
+            # <_cos> is a negated cosine
+            _cos = -vector.unitVector.dot(vector.prev.unitVector)
+            i = bisect_left(collectionInfo[1], _cos, key=itemgetter(1))
+            if not i and (_cos - collectionInfo[1][i-1][1]) < (collectionInfo[1][i][1] - _cos):
+                i -= 1
+            obj = collectionInfo[1][i][0]
+        else:
+            #
+            # concave angle of the corner
+            #
+            _cos = vector.unitVector.dot(vector.prev.unitVector)
         return
