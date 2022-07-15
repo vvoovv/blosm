c8cca65ea244fd1b8d751ad481bf1ce62271e332
Vladimir Elistratov <prokitektura+dev@gmail.com>
2022-07-15 22:28:11 +0300

    fixed a bug with makePolygon() for relations

--- parse/osm/relation/multipolygon.py
+++ parse/osm/relation/multipolygon.py
@@ -439,6 +439,10 @@ class Multipolygon(Relation):
         """
         Make a polygon out of the multipolygon with holes
         """
+        if self.t == parse.polygon:
+            # It's already a polygon. We have nothing to do for it.
+            return
+        
         # get the outer linestring
         for _l in self.ls:
             if _l.role is Osm.outer:
