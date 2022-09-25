f2ce249ae19265ab5eee55bacdeadf8183f82eca
Vladimir Elistratov <prokitektura+dev@gmail.com>
2022-09-25 18:51:10 +0300

    reverted setup/mpl_blosm.py

--- setup/mpl_blosm.py
+++ setup/mpl_blosm.py
@@ -1,27 +1,13 @@
 #from manager import BaseManager, Linestring, Polygon, PolygonAcceptBroken
+from setup import Setup
 from building.manager import BaseBuildingManager
-from way.manager import WayManager, RoadPolygonsManager
 from mpl.renderer.facade_classification import \
     BuildingVisibilityRender, WayVisibilityRenderer, BuildingClassificationRender, BuildingFeatureRender
 from mpl.renderer import BuildingBaseRenderer
-from mpl.renderer.road_polygons import RoadPolygonsRenderer
 from action.facade_visibility import FacadeVisibilityOther
-from action.facade_classification import FacadeClassification
-from action.feature_detection import FeatureDetection
-from action.curved_features import CurvedFeatures
-from action.straight_angles import StraightAngles
 from action.skip_features_again import SkipFeaturesAgain
-from action.gn_streets import StreetsForGN
 
 #from manager.logging import Logger
-
-
-def skip(tags, e):
-    if tags.get("area") == "yes" or tags.get("tunnel") == "yes" or\
-    tags.get("ice_road") == "yes" or "indoor" in tags:
-        e.valid = False
-        return True
-    return False
 
 
 def setup(app, osm):
@@ -39,13 +25,13 @@ def setup(app, osm):
     app.argParserExtra.add_argument("--showFeatureSymbols", action='store_true', help="Show a symbol for each unskipped polygon vector. The symbol is used for pattern matching", default=False)
     app.argParserExtra.add_argument("--simplifyPolygons", action='store_true', help="Simplify polygons with the detected features", default=False)
     app.argParserExtra.add_argument("--restoreFeatures", action='store_true', help="Restore simplified features", default=False)
+    app.argParserExtra.add_argument("--wayClustering", action='store_true', help="Create way clusters", default=False)
     app.argParserExtra.add_argument("--simplifyPolygonsAgain", action='store_true', help="Restore the features and simplify the polygons again", default=False)
-    app.argParserExtra.add_argument("--gnStreets", action='store_true', help="Create streets and intersections for GN", default=False)
     
     # parse the newly added command line arguments
     app.parseArgs()
     classifyFacades = getattr(app, "classifyFacades", False)
-    facadeVisibility = getattr(app, "classification", False)
+    facadeVisibility = getattr(app, "facadeVisibility", False)
     showAssoc = getattr(app, "showAssoc", False)
     showIDs = getattr(app, "showIDs", False)
     
@@ -55,41 +41,19 @@ def setup(app, osm):
     
     simplifyPolygons = getattr(app, "simplifyPolygons", False)
     restoreFeatures = getattr(app, "restoreFeatures", False)
-
+    
     simplifyPolygonsAgain = getattr(app, "simplifyPolygonsAgain", False)
-
-    gnStreets = getattr(app, "gnStreets", False)
     
-    # managers
     
-    buildings = wayManager = gnStreetsManager = None
+    setup = Setup(app, osm)
     
-    #linestring = Linestring(osm)
-    #polygon = Polygon(osm)
-    #polygonAcceptBroken = PolygonAcceptBroken(osm)
+    # create managers
     
-    # conditions for point objects in OSM
-    #osm.addNodeCondition(
-    #    lambda tags, e: tags.get("natural") == "tree",
-    #    "trees",
-    #    None,
-    #    BaseNodeRenderer(app, path, filename, collection)
-    #)
-    
-    if app.highways or app.railways:
-        osm.addCondition(skip)
-        
-        wayManager = WayManager(osm, app)
-        
-        if gnStreets:
-            wayManager.addRenderer(RoadPolygonsRenderer())
-            # buildings.addAction(CurvedFeatures())
-            # buildings.addAction(StraightAngles())
-        else:
-            wayManager.addRenderer(WayVisibilityRenderer(showIDs=showIDs))
+    wayManager = setup.getWayManager()
     
     if app.buildings:
-        buildings = BaseBuildingManager(osm, app, None, None)
+        buildings = setup.buildingManager = BaseBuildingManager(osm, app, None, None)
+        
         buildings.setRenderer(
             BuildingClassificationRender(sideFacadeColor=app.sideFacadeColor, showAssoc=showAssoc,showIDs=showIDs)\
                 if classifyFacades else (\
@@ -104,24 +68,16 @@ def setup(app, osm):
                     )
                 )
         )
-        
+                
         if detectFeatures:
-            buildings.addAction(CurvedFeatures())
-            buildings.addAction(StraightAngles())
-            #buildings.addAction(FeatureDetection(simplifyPolygons))
+            setup.detectFeatures(simplifyPolygons)
         
-        if facadeVisibility or classifyFacades:
-            buildings.addAction(FacadeVisibilityOther())
+        if facadeVisibility:
+            setup.facadeVisibility(FacadeVisibilityOther())
         
         if classifyFacades:
-            buildings.addAction(FacadeClassification())
+            setup.classifyFacades(FacadeVisibilityOther())
         
-        osm.addCondition(
-            lambda tags, e: "building" in tags,
-            "buildings", 
-            buildings
-        )
-
         # the code below is for a test
         if simplifyPolygonsAgain:
             buildings.addAction(
@@ -131,94 +87,15 @@ def setup(app, osm):
                     setup.getFeatureDetectionAction()
                 )
             )
-
-
-    if app.highways:
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") in ("motorway", "motorway_link"),
-            "roads_motorway",
-            wayManager
-        )
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") in ("trunk", "trunk_link"),
-            "roads_trunk",
-            wayManager
-        )
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") in ("primary", "primary_link"),
-            "roads_primary",
-            wayManager
-        )
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") in ("secondary", "secondary_link"),
-            "roads_secondary",
-            wayManager
-        )
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") in ("tertiary", "tertiary_link"),
-            "roads_tertiary",
-            wayManager
-        )
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") == "unclassified",
-            "roads_unclassified",
-            wayManager
-        )
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") in ("residential", "living_street"),
-            "roads_residential",
-            wayManager
-        )
-        # footway to optimize the walk through conditions
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") in ("footway", "path"),
-            "paths_footway",
-            wayManager
-        )
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") == "service",
-            "roads_service",
-            wayManager
-        )
-        # filter out pedestrian areas for now
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") == "pedestrian" and not tags.get("area") and not tags.get("area:highway"),
-            "roads_pedestrian",
-            wayManager
-        )
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") == "track",
-            "roads_track",
-            wayManager
-        )
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") == "steps",
-            "paths_steps",
-            wayManager
-        )
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") == "cycleway",
-            "paths_cycleway",
-            wayManager
-        )
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") == "bridleway",
-            "paths_bridleway",
-            wayManager
-        )
-        osm.addCondition(
-            lambda tags, e: tags.get("highway") in ("road", "escape", "raceway"),
-            "roads_other",
-            wayManager
-        )
-
-    if app.railways:
-        osm.addCondition(
-            lambda tags, e: tags.get("railway") in ("rail", "tram", "subway", "light_rail", "funicular", "monorail"),
-            "railways",
-            wayManager.getRailwayManager()
-        )
+        
+        setup.buildings()
     
-    if gnStreets:
-        gnStreetsManager = RoadPolygonsManager(osm, app)
-        gnStreetsManager.addAction(StreetsForGN())
+    if app.highways or app.railways:
+        setup.skipWays()
+        wayManager.addRenderer(WayVisibilityRenderer(showIDs=showIDs))
+    
+    if app.highways:
+        setup.roadsAndPaths()
+    
+    if app.railways:
+        setup.railways()
