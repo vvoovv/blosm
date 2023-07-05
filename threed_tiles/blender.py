50cfa9e5e966697d45ef8f69943905e08abb4174
Vladimir Elistratov <prokitektura+dev@gmail.com>
2023-07-05 18:06:50 +0200

    rewrote 3D Tiles geometrical transformations

--- threed_tiles/blender.py
+++ threed_tiles/blender.py
@@ -5,6 +5,7 @@ import re
 from operator import itemgetter
 
 import bpy
+from mathutils import Matrix
 
 from util.blender import createEmptyObject, createCollection
 
@@ -30,61 +31,46 @@ class BlenderRenderer:
             self.collection = None
             return
         
-        context = bpy.context
-        # find avarage height of <self.importedObjects>
-        #height = sum(obj.location[2] for obj in self.importedObjects)/len(self.importedObjects)
+        #
+        # tranformation matrix
+        #
         centerCoords = manager.fromGeographic(manager.centerLat, manager.centerLon, 0.)
-        
-        parentObject = createEmptyObject(self.threedTilesName, (0., 0., 0.), collection=self.collection)
-        
-        for obj in self.importedObjects:
-            obj.parent = parentObject
-        #    obj.matrix_parent_inverse = parentObject.matrix_world.inverted()
-        
-        parentObject.rotation_mode = 'ZXY'
-        parentObject.rotation_euler[2] = radians(-90. - manager.centerLon)
+        # lat = radians(manager.centerLat - 90.) # gives incorrect result for the expression below
         lat = atan(centerCoords[2]/sqrt(centerCoords[0]*centerCoords[0] + centerCoords[1]*centerCoords[1]))
-        # <radians(manager.centerLat - 90.)> gives incorrect result
-        # for the expression below
-        parentObject.rotation_euler[0] = lat-pi/2.
+        # Bring the mesh to the north pole with rotations around Z and X axes,
+        # then move it to the center of the coordinate reference system
+        matrix = Matrix.Rotation(lat-pi/2., 4, 'X') @ Matrix.Rotation(radians(-90. - manager.centerLon), 4, 'Z')
         
-        bpy.ops.object.select_all(action='DESELECT')
-        parentObject.select_set(True)
-        bpy.ops.object.transform_apply(location=False, rotation=True, scale=False)
-        parentObject.select_set(False)
+        locationsAtNorthPole = [(matrix @ obj.location) for obj in self.importedObjects]
         
-        for obj in self.importedObjects:
-            obj.parent = None
-            obj.select_set(True)
-        
-        bpy.ops.object.transform_apply(location=False, rotation=True, scale=False)
-        
-        heightOffset = min(obj.location[2] for obj in self.importedObjects)\
+        # find the lowest Z-coordinate if <self.calculateHeightOffset>
+        heightOffset = min(location[2] for location in locationsAtNorthPole)\
             if self.calculateHeightOffset else\
             self.heightOffset
         if self.calculateHeightOffset:
             self.heightOffset = heightOffset
         
-        for obj in self.importedObjects:
-            obj.location[2] -= heightOffset
+        bpy.ops.object.select_all(action='DESELECT')
         
-        # join the selected objects
         if self.join3dTilesObjects:
-            bpy.ops.object.join()
-            joinedObject = self.importedObjects[-1]
-            joinedObject.name = self.threedTilesName
-            bpy.data.objects.remove(parentObject, do_unlink=True)
-            context.view_layer.objects.active = joinedObject
-            bpy.ops.object.mode_set(mode='EDIT')
-            bpy.ops.mesh.select_all(action='SELECT')
-            bpy.ops.mesh.remove_doubles()
-            bpy.ops.object.mode_set(mode='OBJECT')
-        else:
             for obj in self.importedObjects:
-                obj.parent = parentObject
-                obj.select_set(False)
+                obj.select_set(True)
+            self.joinObjects()
+            
+            joinedObject = self.importedObjects[-1]
+            location = locationsAtNorthPole[-1]
+            location[2] -= heightOffset
+            joinedObject.matrix_local = Matrix.Translation(location) @ matrix
+        else:
+            for obj, location in zip(self.importedObjects, locationsAtNorthPole):
+                location[2] -= heightOffset
+                obj.matrix_local = Matrix.Translation(location) @ matrix
+                obj.select_set(True)
         
-        context.scene.blosm.copyright = "; ".join(
+        bpy.ops.object.transform_apply(location=False, rotation=True, scale=False) 
+        bpy.ops.object.select_all(action='DESELECT')
+        
+        bpy.context.scene.blosm.copyright = "; ".join(
             entry[0] for entry in reversed(
                 sorted(
                     self.copyrightHolders.items(), key=itemgetter(1)
@@ -124,9 +110,10 @@ class BlenderRenderer:
             bpy.ops.import_scene.gltf(filepath=filePath)
             removeFile(filePath)
         
-        self.collection.objects.link(context.object)
-        self.importedObjects.append(context.object)
-        context.scene.collection.objects.unlink(context.object)
+        importedObject = context.object
+        self.collection.objects.link(importedObject)
+        self.importedObjects.append(importedObject)
+        context.scene.collection.objects.unlink(importedObject)
     
     def processCopyrightInfo(self, info):
         for copyrightHolder in info.split(';'):
@@ -134,3 +121,13 @@ class BlenderRenderer:
             if not copyrightHolder in self.copyrightHolders:
                 self.copyrightHolders[copyrightHolder] = 0
             self.copyrightHolders[copyrightHolder] += 1
+    
+    def joinObjects(self):
+        bpy.ops.object.join()
+        joinedObject = self.importedObjects[-1]
+        joinedObject.name = self.threedTilesName
+        bpy.context.view_layer.objects.active = joinedObject
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_all(action='SELECT')
+        bpy.ops.mesh.remove_doubles()
+        bpy.ops.object.mode_set(mode='OBJECT')
